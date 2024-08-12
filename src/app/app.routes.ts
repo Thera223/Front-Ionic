b7e83120d2d1b5788ebb0833704afedb8531dc1e
Harouna-Ness <harouna.hkdk@gmail.com>
2024-08-12 15:54:44 +0000

    afficher produits selon sous-categorie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,14 @@ export const routes: Routes = [
     path: 'tab4',
     loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
   },
+  {
+    path: 'categorie',
+    loadComponent: () => import('./categorie/categorie.page').then( m => m.CategoriePage)
+  },
+  {
+    path: 'detail/:id',
+    loadComponent: () => import('./detail/detail.page').then( m => m.DetailPage)
+  },
+
+
 ];
