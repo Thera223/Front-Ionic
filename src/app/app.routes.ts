a944ee5c67f92e5162f073ea597ff7821b4d559a
Aboudou <doolcoumba@gmail.com>
2024-08-13 15:16:03 +0000

    mon push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,20 @@ export const routes: Routes = [
     path: 'tab4',
     loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
   },
+  {
+    path: 'confirmation',
+    loadComponent: () => import('./confirmation/confirmation.page').then( m => m.ConfirmationPage)
+  },
+  {
+    path: 'livraison',
+    loadComponent: () => import('./livraison/livraison.page').then( m => m.LivraisonPage)
+  },
+  {
+    path: 'mode',
+    loadComponent: () => import('./mode/mode.page').then( m => m.ModePage)
+  },
+  {
+    path: 'paiement',
+    loadComponent: () => import('./paiement/paiement.page').then( m => m.PaiementPage)
+  },
 ];
